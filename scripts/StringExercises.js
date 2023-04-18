36bb57f0aec1a417f5a75cf48d07d1961f7997e7
unknown <GrahamE@ywcanwil.org>
2023-04-18 15:09:46 -0500

    New Exercises using conditionals applied to Strings

---
+++ scripts/StringExercises.js
@@ -0,0 +1,90 @@
+// * STRING EXERCISES
+
+/**
+ * Given a string, inputString, return the same string reversed.
+ * Remember: Strings are immutable - you cannot alter them like you can with arrays.
+ * 
+ * 
+ * @param {String} inputString 
+ * @returns {String} A reversed string of inputString
+ */
+function reverseAString(inputString) {
+    // TODO Finish the function
+}
+
+/**
+ * Given an array of strings, find the length of the shortest string in the array.
+ * Return the shortest string's length.
+ * 
+ * @param {Array<String>} strings 
+ * @returns {Number} The length of the shortest string
+ */
+function findTheShortestString(strings) {
+    // TODO Finish the funciton
+}
+
+/**
+ * Given a string, find and return the number of vowels.
+ * For the purposes of this function, vowels are considered: a, e, i, o, u.
+ * 
+ * @param {String} string - A random String with any number of characters
+ * @returns {Number} The number of vowels in the string
+ */
+function findNumberOfVowels(string) {
+    // TODO Finish the function
+}
+
+/**
+ * Given an array of strings, return a new array with only the strings that are
+ * greater than or equal to a random length, len.
+ * 
+ * @param {Array<String>} stringArr - An array of strings
+ * @param {Number} len - A number
+ * @returns {Array<String>} Array of Strings that were all greater than or equal to len
+ */
+function greaterThanLength(stringArr, len) {
+    // TODO Finish the function
+}
+
+/**
+ * Given a String, return a new string with all the vowels removed.
+ * 
+ * @param {String} string 
+ * @returns {String}
+ */
+function removeVowels(string) {
+    // TODO Finish the function
+}
+
+/**
+ * Given an array of strings, return a new array with all the strings sorted in alphabetical order.
+ * 
+ * @param {Array<String>} stringArr 
+ * @returns {Array<String>} An array of sorted strings
+ */
+function sortAllStrings(stringArr) {
+    // TODO Finish the function
+}
+
+/**
+ * Given an array of Strings, return a new array where all the arrays' length 
+ * are sorted from smallest to largest
+ * 
+ * Ex: ["hi", "i", "like", "javascript"] => ["i", "hi", "like", "javascript"]
+ * 
+ * @param {Array<String>} stringArr 
+ * @returns {Array<String} An array of String sorted by length
+ */
+function sortArrayByLength(stringArr) {
+    // TODO Finish the function
+}
+
+/**
+ * Given a string, return a new string with all vowels replaced with the letter "o".
+ * 
+ * @param {String} string 
+ * @returns {String} String with all vowels as O
+ */
+function replaceVowelWithO(string) {
+    // TODO Finish the function
+}
