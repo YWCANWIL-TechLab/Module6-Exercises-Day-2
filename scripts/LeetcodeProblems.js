d511d09d46b41596be30a0749f4328830df7bf65
unknown <GrahamE@ywcanwil.org>
2023-04-18 15:28:35 -0500

    Complete this last

---
+++ scripts/LeetcodeProblems.js
@@ -0,0 +1,21 @@
+// * LEETCODE EXERCISES
+
+// ! Leetcode.com is a website software engineers use to practice for technical interviews. To land a job working as a Web Developer,
+// ! you may have to complete some problems like these.
+
+// * If you are interested, leetcode.com is a great way to improve your skills with strings, arrays, conditionals, and loops
+// * as well as preparing for techncial interviews.
+
+/**
+ * Given two strings word1 and word2, merge the strings to a new string by adding them to the merged string in alternate order.
+ * If one string is longer than the other string, append the extra letters to the end of the merged string
+ * 
+ * @param {String} word1 
+ * @param {String} word2 
+ * @returns {String} A string of word1 and word2 merged together
+ */
+function leetcode1768(word1, word2) {
+    // TODO Finish the function
+
+}
+
