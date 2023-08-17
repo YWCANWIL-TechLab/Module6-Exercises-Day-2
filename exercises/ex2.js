f8c359e052f2b1a47636a1aee66ce3bc3fd9e45d
Graham Eichsteadt <GrahamE@ywcanwil.org>
2023-08-17 11:52:41 -0500

    Updated Exercise Formatting

---
+++ exercises/ex2.js
@@ -0,0 +1,110 @@
+// * STRING EXERCISES
+
+/**
+ * Given a string, inputString, return the same string reversed.
+ * Remember: Strings are immutable - you cannot alter them like you can with 
+ * arrays.
+ * 
+ * Ex: "hello world" => "dlrow olleh"
+ * 
+ * * Hint: A standard for loop will allow you to iterate in either direction. 
+ * * You can set your iterator to whatever you would like, so to go backwards 
+ * * you can set it to the last index of your string, then reduce it by one for 
+ * * each character.
+ * 
+ * @param {String} inputString 
+ * @returns {String} A reversed string of inputString
+ */
+function reverseAString(inputString) {
+    // TODO Finish the function
+}
+
+/**
+ * Given an array of strings, find the length of the shortest string in the array.
+ * Return the shortest string's length.
+ * 
+ * Ex: ["This", "is", "your", "example"] => 2
+ * 
+ * @param {Array<String>} strings 
+ * @returns {Number} The length of the shortest string
+ */
+function findTheShortestString(strings) {
+    // TODO Finish the function
+}
+
+/**
+ * Given a string, find and return the number of vowels.
+ * For the purposes of this function, vowels are considered: a, e, i, o, u.
+ * 
+ * Ex: "Hello world" => 3
+ * 
+ * @param {String} string - A random String with any number of characters
+ * @returns {Number} The number of vowels in the string
+ */
+function findNumberOfVowels(string) {
+    // TODO Finish the function
+}
+
+/**
+ * Given an array of strings, return a new array with only the strings that are
+ * greater than or equal to a random length, len.
+ * 
+ * Ex: len = 5, ["I", "am", "your", "favorite", "array"] => ["favorite", "array"]
+ * 
+ * @param {Array<String>} stringArr - An array of strings
+ * @param {Number} len - A number
+ * @returns {Array<String>} Array of Strings that were all greater than or equal to len
+ */
+function greaterThanLength(stringArr, len) {
+    // TODO Finish the function
+}
+
+/**
+ * Given a String, return a new string with all the vowels removed.
+ * For the purposes of this exercises, vowels are considered: a, e, i, o, u
+ * 
+ * Ex: "Boy howdy!" => "By hwdy"
+ * 
+ * @param {String} string 
+ * @returns {String}
+ */
+function removeVowels(string) {
+    // TODO Finish the function
+}
+
+/**
+ * Given an array of strings, return a new array with all the strings sorted in alphabetical order.
+ * 
+ * Ex: ["grape", "banana", "dolphin", "zebra"] => ["banana", "dolphin", "grape", "zebra"]
+ * 
+ * @param {Array<String>} stringArr 
+ * @returns {Array<String>} An array of sorted strings
+ */
+function sortAllStrings(stringArr) {
+    // TODO Finish the function
+}
+
+/**
+ * Given an array of Strings, return a new array where all the arrays' length 
+ * are sorted from smallest to largest
+ * 
+ * Ex: ["hi", "i", "like", "javascript"] => ["i", "hi", "like", "javascript"]
+ * 
+ * @param {Array<String>} stringArr 
+ * @returns {Array<String} An array of String sorted by length
+ */
+function sortArrayByLength(stringArr) {
+    // TODO Finish the function
+}
+
+/**
+ * Given a string, return a new string with all vowels replaced with the letter "o".
+ * 
+ * Ex: "This is written using JavaScript" => "Thos os wrotton osong JovoScropt"
+ * 
+ * @param {String} string 
+ * @returns {String} String with all vowels as O
+ */
+function replaceVowelWithO(string) {
+    // TODO Finish the function
+}
